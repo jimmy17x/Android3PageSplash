712455c23f98ca2fc1e40106f06537dddb8e3d6c
jimmy17x <divyanshu17x@gmail.com>
2015-08-04 19:49:17 -0700

    added animation to images in splash screen

--- src/me/jimmyx/got/Home.java
+++ src/me/jimmyx/got/Home.java
@@ -1,12 +1,29 @@
 package me.jimmyx.got;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Home extends Activity {
+	private static int backButtonCount;
+
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home);
+	}
+	public void onBackPressed() {
+		if (backButtonCount >= 1) {
+			Intent intent = new Intent(Intent.ACTION_MAIN);
+			intent.addCategory(Intent.CATEGORY_HOME);
+			intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			startActivity(intent);
+			finish();
+		} else {
+			Toast.makeText(this, "Press the back button to exit :(",
+					Toast.LENGTH_SHORT).show();
+			backButtonCount++;
+		}
 	}
 }
